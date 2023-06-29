203569df1cccc53bcc02e2adc5b507a9ef05c2ce
matheus <matheus.augusto@actionlabs.com.br>
2023-06-29 17:12:31 -0300

    Update

--- src/pages/HealthUnit/HealthUnit.js
+++ src/pages/HealthUnit/HealthUnit.js
@@ -1,97 +1,313 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, FlatList, Modal, Button } from 'react-native';
 import * as Animatable from 'react-native-animatable';
-
 import { useNavigation } from '@react-navigation/native';
 import { loadUserInfo } from '../../libs/storage';
 import { Load } from '../../components/Load';
 
 export default function HealthUnit() {
-
   const navigation = useNavigation();
-  const [loading, setLoading] = useState(true)
-  const [info, setInfo] = useState({})
-
+  const [loading, setLoading] = useState(true);
+  const [info, setInfo] = useState({});
+  const [notifications, setNotifications] = useState([]);
+  const [selectedNotification, setSelectedNotification] = useState(null);
+  const [modalVisible, setModalVisible] = useState(false);
+  const [disableButton, setDisableButton] = useState(false);
+  const [counter, setCounter] = useState(5);
 
   useEffect(() => {
     async function loadStoragedData() {
-        const userInfo = await loadUserInfo()
-        setInfo(userInfo)
-        setLoading(false)
-    }
-    loadStoragedData()
-  })
+      const userInfo = await loadUserInfo();
+      setInfo(userInfo);
 
-  if(loading)
-        return <Load />
+      // Mocking the API call with static data.
+      const notifications = [
+        {
+          id: 1,
+          title: 'Consulta agendada',
+          content: 'Sua consulta com o Dr. Silva está agendada para 1 de Julho, às 14:00.',
+          date: '26 de Junho, 2023',
+          buttonLabel: 'Confirmar Presença',
+          buttonLabelTwo: 'Desmarcar'
+        },
+        {
+          id: 2,
+          title: 'Resultado de exame',
+          content: 'O resultado do seu exame de sangue já está disponível. Por favor, agende uma consulta para a revisão.',
+          date: '25 de Junho, 2023',
+          buttonLabel: '',
+          buttonLabelTwo: ''
+        },
+        {
+          id: 3,
+          title: 'Vacinação agendada',
+          content: 'Lembrete: sua vacinação contra a gripe está agendada para amanhã. Não se esqueça de levar seu cartão de vacinação.',
+          date: '24 de Junho, 2023',
+          buttonLabel: 'Confirmar Presença',
+          buttonLabelTwo: 'Desmarcar'
+        },
+        {
+          id: 4,
+          title: 'Alteração de horário',
+          content: 'Informamos que houve uma alteração no horário da sua consulta. Agora será às 15:30.',
+          date: '23 de Junho, 2023',
+          buttonLabel: 'Confirmar Presença',
+          buttonLabelTwo: 'Desmarcar'
+        },
+        {
+          id: 5,
+          title: 'Confirmação de agendamento',
+          content: 'Seu agendamento para a cirurgia foi confirmado. Por favor, compareça ao hospital no dia e horário marcados.',
+          date: '22 de Junho, 2023',
+          buttonLabel: 'Confirmar Presença',
+          buttonLabelTwo: 'Desmarcar'
+        },
+        {
+          id: 6,
+          title: 'Consulta cancelada',
+          content: 'Infelizmente, a consulta agendada para amanhã foi cancelada devido a imprevistos. Entre em contato para reagendar.',
+          date: '21 de Junho, 2023',
+          buttonLabel: '',
+          buttonLabelTwo: ''
+        },
+        {
+          id: 7,
+          title: 'Receita médica disponível',
+          content: 'Sua receita médica já está disponível para retirada na recepção do hospital.',
+          date: '20 de Junho, 2023',
+          buttonLabel: '',
+          buttonLabelTwo: ''
+        },
+        {
+          id: 8,
+          title: 'Alteração no local da consulta',
+          content: 'A consulta com o Dr. Almeida será realizada no novo prédio do hospital.',
+          date: '19 de Junho, 2023',
+          buttonLabel: 'Confirmar Presença',
+          buttonLabelTwo: 'Desmarcar'
+        },
+      ];
+      setNotifications(notifications);
+
+      setLoading(false);
+    }
+
+    loadStoragedData();
+  }, []);
+
+  useEffect(() => {
+    if (counter > 0 && disableButton) {
+      const timer = setTimeout(() => {
+        setCounter(counter - 1);
+      }, 1000);
+      return () => clearTimeout(timer);
+    } else if (counter === 0) {
+      setDisableButton(false);
+    }
+  }, [counter, disableButton]);
+
+  function handleNotificationPress(notification) {
+    setSelectedNotification(notification);
+    setModalVisible(true);
+    setDisableButton(true);
+    setCounter(5);
+  }
+
+  function handleConfirmPress() {
+    // Handle the "confirm" button press, for example mark the notification as read.
+    const updatedNotifications = notifications.filter(
+      (item) => item.id !== selectedNotification.id
+    );
+    setNotifications(updatedNotifications);
+    setModalVisible(false);
+  }
+
+  if (loading) return <Load />;
 
   return (
     <Animatable.View style={styles.container}>
+      <Animatable.View style={styles.containerForm} delay={600} animation="fadeInUp">
+        <Text style={styles.title}>Suas notificações</Text>
 
-      <Animatable.View  style={styles.containerForm} delay={600} animation="fadeInUp">
-        <Text style={styles.title}>Seu Nome {info?.name}</Text>
+        <FlatList
+          data={notifications}
+          keyExtractor={(item) => item.id.toString()}
+          renderItem={({ item }) => (
+            <TouchableOpacity
+              onPress={() => handleNotificationPress(item)}
+              style={styles.notificationItem}
+            >
+              <Text style={styles.notificationTitle}>{item.title}</Text>
+              <Text style={styles.notificationDate}>{item.date}</Text>
+            </TouchableOpacity>
+          )}
+        />
 
-        <TouchableOpacity 
-        style={styles.button} 
-        onPress={ () => navigation.navigate('SignIn') }>
-          <Text style={styles.buttonText}>Acessar</Text>
-        </TouchableOpacity>
+        <Modal animationType="slide" transparent={true} visible={modalVisible}>
+          <View style={styles.centeredView}>
+            <View style={styles.modalView}>
+              <Text style={styles.modalTitle}>{selectedNotification?.title}</Text>
+              <Text style={styles.modalContent}>{selectedNotification?.content}</Text>
+
+              <View style={styles.containerButton}>
+                {selectedNotification?.buttonLabel && selectedNotification?.buttonLabelTwo ? (
+                  <>
+                    <TouchableOpacity
+                      style={[
+                        styles.buttonConfirm,
+                        disableButton && styles.disabledButton,
+                      ]}
+                      onPress={handleConfirmPress}
+                      disabled={disableButton}
+                    >
+                      <Text style={styles.buttonText}>
+                        {disableButton
+                          ? `${selectedNotification?.buttonLabel} ${counter}s`
+                          : selectedNotification?.buttonLabel}
+                      </Text>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity
+                      style={[
+                        styles.buttonCancel,
+                        disableButton && styles.disabledButton,
+                      ]}
+                      onPress={handleConfirmPress}
+                      disabled={disableButton}
+                    >
+                      <Text style={styles.buttonText}>
+                        {disableButton
+                          ? `${selectedNotification?.buttonLabelTwo} ${counter}s`
+                          : selectedNotification?.buttonLabelTwo}
+                      </Text>
+                    </TouchableOpacity>
+                  </>
+                ) : (
+                  <TouchableOpacity style={styles.buttonOk} onPress={handleConfirmPress}>
+                    <Text style={styles.buttonText}>Ok</Text>
+                  </TouchableOpacity>
+                )}
+              </View>
+            </View>
+          </View>
+        </Modal>
       </Animatable.View>
     </Animatable.View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex:1,
-    backgroundColor: '#fff',
+    flex: 1,
+    backgroundColor: '#f0f0f5',
   },
-
-  containerLogo: {
-    flex:2,
-    backgroundColor: '#179ee8',
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-
-  textJC: {
-    color: '#fff',
-  },
-
   containerForm: {
-    flex:1,
-    backgroundColor: '#fff',
+    flex: 1,
+    backgroundColor: '#f0f0f5',
     borderTopLeftRadius: 25,
     borderTopRightRadius: 25,
     paddingStart: '5%',
-    paddingEnd: '5%'
+    paddingEnd: '5%',
   },
-
   title: {
     fontSize: 24,
     fontWeight: 'bold',
     marginTop: 28,
     marginBottom: 12,
+    color: '#333',
   },
-
-  text: {
-    color: '#A1A1A1'
+  notificationItem: {
+    backgroundColor: '#fff',
+    borderRadius: 10,
+    padding: 20,
+    marginBottom: 10,
+    borderWidth: 1,
+    borderColor: '#ddd',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 1 },
+    shadowOpacity: 0.8,
+    shadowRadius: 2,
+    elevation: 5,
   },
-  button: {
-    position: 'absolute',
-    backgroundColor: '#179ee8',
-    borderRadius: 50,
-    paddingVertical: 8,
-    width: '60%',
-    alignSelf: 'center',
-    bottom: '15%',
+  notificationTitle: {
+    fontSize: 18,
+    color: '#333',
+    fontWeight: 'bold',
+  },
+  notificationDate: {
+    fontSize: 14,
+    color: '#666',
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
     alignItems: 'center',
-    justifyContent: 'center'
+    marginTop: 22,
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  modalTitle: {
+    marginBottom: 15,
+    textAlign: 'center',
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  modalContent: {
+    marginBottom: 15,
+    textAlign: 'center',
+  },
+  containerButton: {
+    marginTop: 20,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  buttonConfirm: {
+    backgroundColor: '#8bc34a',
+    borderRadius: 10,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    width: 150,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginRight: 10,
+  },
+  buttonOk: {
+    backgroundColor: '#179ee8',
+    borderRadius: 10,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    width: 150,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buttonCancel: {
+    backgroundColor: '#FF0000',
+    borderRadius: 10,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    width: 150,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  disabledButton: {
+    backgroundColor: '#aaa',
   },
   buttonText: {
     fontSize: 18,
     color: '#fff',
-    fontWeight: 'bold'
-  }
-
-})
+    fontWeight: 'bold',
+  },
+});
--- src/pages/ScheduleInfo/ScheduleInfo.js
+++ src/pages/ScheduleInfo/ScheduleInfo.js
@@ -1,17 +1,22 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, Image, TouchableOpacity, ScrollView } from 'react-native';
 import * as Animatable from 'react-native-animatable';
 import { FontAwesome5 } from '@expo/vector-icons';
 
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation, useRoute } from '@react-navigation/native';
 import { loadUserInfo } from '../../libs/storage';
 import { Load } from '../../components/Load';
+import api from '../../services/api';
 
 export default function ScheduleInfo() {
 
+  const route = useRoute();
+  const { id, type, description } = route.params;
   const navigation = useNavigation();
-  const [loading, setLoading] = useState(true)
-  const [info, setInfo] = useState({})
+  const [loading, setLoading] = useState(true);
+  const [info, setInfo] = useState({});
+  const [data, setData] = useState({});
+
 
 
   useEffect(() => {
@@ -21,7 +26,18 @@ export default function ScheduleInfo() {
         setLoading(false)
     }
     loadStoragedData()
-  })
+  }, [])
+
+  useEffect(() => {
+    async function handleGetSchedules() {
+      const { data } = await api.get(`getInfoAgendamento?idAgendamento=1`);
+      setData(data);
+      setLoading(false);
+    }
+    if (info.idPaciente) {
+      handleGetSchedules();
+    }
+  }, [info]);
 
   if(loading)
         return <Load />
@@ -30,7 +46,7 @@ export default function ScheduleInfo() {
     <Animatable.View style={styles.container} animation="fadeInUp">
       <View style={styles.containerUser}>
         <View style={styles.contentUser}>
-          <Text style={styles.titleUser}>INFOOOO</Text>  
+          <Text style={styles.titleUser}>{info.name}</Text>  
           <TouchableOpacity 
             style={styles.buttonUser}
             activeOpacity={0.9} 
@@ -47,42 +63,63 @@ export default function ScheduleInfo() {
         <View style={styles.contentUser}>
           <Text style={styles.titleUser}>Meus Agendamentos</Text>  
         </View>
-        <View style={styles.containerProcedures}>
-          <Text style={styles.textProcedures}>Procedures</Text>
-          <View style={styles.contentProcedures}>
-            <View>
-              <TouchableOpacity
-                style={styles.buttonProcedures}
-                onPress={() => navigation.navigate('ExamResults')}>
-                <Text style={styles.buttonText}>Clínico Geral</Text>
-              </TouchableOpacity>
-            </View>
-            <View>
-              <TouchableOpacity
-                style={styles.buttonProcedures}
-                onPress={() => navigation.navigate('')}>
-                <Text style={styles.buttonText}>Ortopedia</Text>
-              </TouchableOpacity>
-            </View>
-          </View>
-        </View>
-        <View style={styles.containerExams}>
-          <Text style={styles.textExams}>Exames</Text>
-          <View style={styles.contentExams}>
-            <View>
-              <TouchableOpacity
-                style={styles.buttonExams}
-                onPress={() => navigation.navigate('')}>
-                <Text style={styles.buttonText}>Raio-X</Text>
-              </TouchableOpacity>
-            </View>
-          </View>
-        </View>
-        <View style={styles.containerAttention}>
-          <Text style={styles.textPersonalAttention}>ATENÇÃO</Text>
-          <Text style={styles.textAttention}>A marcação de consultas e exames deve ser feito presencialmente e está sujeito a disponibilidade de</Text>
-        </View>
       </View>
+      <ScrollView style={styles.containerMain}>
+        {
+          type === 'procedures' 
+          ?
+            <View style={styles.containerProcedures}>
+              <Text style={styles.textProcedures}>Procedimentos</Text>
+              <View style={styles.descriptionInfo}>
+                <Text style={styles.titleDescription}>{description}</Text>
+                
+              </View>
+              <View style={styles.descriptionInfoLocal}>
+                
+              </View>
+              {
+                data && data.length > 0 && data.status !== 2 ?
+                  data.map(item => (
+                    <View style={styles.descriptionInfoLocal}>
+                      <Text style={styles.textOption}>Data: <Text style={styles.captionUser}>{data[0].dataAgendamento}</Text></Text>
+                      <Text style={styles.textOption}>Turno: <Text style={styles.captionUser}>{data[0].horarioAgendamento}</Text></Text>
+                      <Text style={styles.textOption}>Local: <Text style={styles.captionUser}>{data[0].nomeUnidade}</Text></Text>
+                    </View>
+                  ))
+                :
+                <View>          
+                   <Load color="#000"/>
+                </View>
+              }
+            </View> 
+          :
+            <View style={styles.containerExams}>
+              <Text style={styles.textExams}>Exames</Text>
+              <View style={styles.descriptionInfo}>
+                <Text style={styles.titleDescription}>{description}</Text>
+                
+              </View>
+              <View style={styles.descriptionInfoLocal}>
+                
+              </View>
+              {
+                data && data.length > 0 && data.status !== 2 ?
+                  data.map((item, index) => (
+                    <View style={styles.descriptionInfoLocal} key={index}>
+                      {/* conteúdo do elemento */}
+                    </View>
+                  ))
+                :
+                <View>
+                  <Load color="#000" />
+                </View>
+              }
+
+            </View>
+        }
+        
+        
+      </ScrollView>
     </Animatable.View>
 
   )
@@ -121,33 +158,58 @@ const styles = StyleSheet.create({
   },
 
   containerProcedures: {
+    justifyContent: 'flex-start',
+    marginTop: 20,
+    marginBottom: 20
+  },
+
+  scrollContent: {
+    flexGrow: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    marginTop: 40,
-    marginBottom: 20,
   },
 
   contentProcedures: {
-    minHeight: 180
+    maxHeight: 140
   },
 
   textProcedures: {
     fontSize: 17,
     fontWeight: 700,
     color: '#00B0F3',
-    alignItems: 'center',
+    alignItems: 'flex-start',
     marginBottom: 10
   },
 
-  containerExams: {
+  descriptionInfo: {
     justifyContent: 'center',
     alignItems: 'center',
-    marginTop: 20,
+    marginTop: 30
+  },
+
+  descriptionInfoLocal: {
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start',
+    marginTop: 30
+  },
+
+  titleDescription: {
+    color: 'rgba(32, 48, 117, 1)',
+    fontStyle: 'normal',
+    fontSize: 25,
+    fontWeight: 700,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+
+  containerExams: {
+    justifyContent: 'flex-start',
+    marginTop: 0,
     marginBottom: 20,
   },
 
   contentExams: {
-    minHeight: 180
+    minHeight: 140
   },
 
   textExams: {
@@ -159,6 +221,12 @@ const styles = StyleSheet.create({
   },
 
   containerAttention: {
+    margin: '6%',
+    marginTop: 0,
+    paddingTop: 0
+  },
+
+  contentAttention: {
     justifyContent: 'center',
     alignItems: 'center'
   },
@@ -173,12 +241,13 @@ const styles = StyleSheet.create({
     fontSize: 15,
     fontWeight: 300,
     color: '#666666',
-    alignItems: 'center'
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 
   buttonProcedures: {
     backgroundColor: '#00B0F3',
-    width: 364,
+    width: 320,
     height: 72,
     borderRadius: 5,
     alignItems: 'center',
@@ -189,7 +258,7 @@ const styles = StyleSheet.create({
 
   buttonExams: {
     backgroundColor: '#DE0788',
-    width: 364,
+    width: 320,
     height: 72,
     borderRadius: 5,
     alignItems: 'center',
@@ -204,6 +273,19 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     color: '#fff',
   },
+
+  textOption: {
+    color: 'rgba(32, 48, 117, 1)',
+    fontStyle: 'normal',
+    fontSize: 18,
+    fontWeight: 700,
+    marginTop: 2,
+    marginBottom: 2,
+  },
+  captionUser: {
+    color: 'rgba(32, 48, 117, 1)',
+    fontStyle: 'normal',
+    fontSize: 17,
+    fontWeight: 300
+  },
 });
-
-
